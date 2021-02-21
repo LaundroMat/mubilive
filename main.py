da2166ae0055f41cecb17d32da296980ea7603f8
Mathieu Dhondt <laundro@gmail.com>
2021-02-21 12:22:33 +0100

    Call # lambdas per country

--- main.py
+++ main.py
@@ -42,52 +42,74 @@ def create_api():
 
 api = create_api()
 
+current_movies = [
+    {
+        'country': 'Ireland',
+        'flag': '🇮🇪',
+        'url': 'https://swksavcqn2.execute-api.eu-west-1.amazonaws.com/MubiLive',
+        'current_movie_id': 0,
+    }, {
+        'country': 'France',
+        'flag': '🇫🇷',
+        'url': 'https://pszitemmtj.execute-api.eu-west-3.amazonaws.com/MubiLive',
+        'current_movie_id': 0
+    }, {
+        'country': 'US',
+        'flag': '🇺🇸',
+        'url': 'https://m4x6b73f0j.execute-api.us-east-1.amazonaws.com/default/MubiLIve',
+        'current_movie_id': 0
+    },
+    {
+        'country': 'Germany',
+        'flag': '🇩🇪',
+        'url': 'https://226nqjv99d.execute-api.eu-central-1.amazonaws.com/default/MubiLive',
+        'current_movie_id': 0
+    },
 
-def check(current_movie_id: int = None):
-    current_movie_id = int(current_movie_id) if current_movie_id else None
+]
 
-    headers = {
-        "Cache-Control": "no-cache",
-        "Pragma": "no-cache"
-    }
 
-    r = requests.get('https://mubi.com/live.json', params={"_": arrow.utcnow().int_timestamp}, headers=headers)
-    data = r.json()
-    logger.debug(data)
-    film_info = data['film_programming']
+def check(current_movies: list):
+    for movie in current_movies:
+        r = requests.get(movie['url'])
+        data = r.json()
+        logger.debug(data)
+        film_info = data['film_programming']
 
-    if film_info['id'] != current_movie_id:
-        logger.debug(f"New movie: {film_info['title']}")
-
-        tweet_body = f'''
-{film_info['title']} ({film_info['directors']}, {film_info['country']}, {film_info['year']}) just started on mubi.com/live when you're in the US.
-
+        if film_info['id'] != movie['current_movie_id']:
+            logger.debug(f"New movie: {film_info['title']}")
+            movie['current_movie_id'] = film_info['id']
+            tweet_body = f'''
+{film_info['title']} ({film_info['directors']}, '''
+            if film_info.get('country'):
+                tweet_body += f"{film_info.get('country')}"
+            tweet_body += f'''
+{film_info['year']}) just started on mubi.com/live in {movie['flag']} {movie['country']}.
+    
 {film_info['excerpt']}
-
-
 '''
-        tweet_img = film_info['stills']['retina']
+            tweet_img = film_info['stills']['retina']
 
-        filename = f"{film_info['title']}.jpg"
-        r = requests.get(tweet_img)
-        if r.status_code == 200:
-            with open(filename, 'wb') as image:
-                for chunk in r:
-                    image.write(chunk)
+            filename = f"{film_info['title']}.jpg"
+            r = requests.get(tweet_img)
+            if r.status_code == 200:
+                with open(filename, 'wb') as image:
+                    for chunk in r:
+                        image.write(chunk)
 
-        parts = tweet_body.split(".")
-        while len(".".join(parts)) > 278:
-            parts.pop()
+            parts = tweet_body.split(".")
+            while len(".".join(parts)) > 255:
+                parts.pop()
 
-        status = ".".join(parts) + "."
-        api.update_with_media(filename, status=status)
-        os.remove(filename)
-    else:
-        print(f"No new movie (yet). Still watching {film_info['title']}.")
+            status = ".".join(parts) + "."
+            api.update_with_media(filename, status=status)
+            os.remove(filename)
+        else:
+            print(f"No new movie for {movie['country']} (yet). Still watching {film_info['title']}.")
 
     time.sleep(SECONDS_BETWEEN_CHECKS)
-    check(film_info['id'])
+    check(current_movies)
 
 
 if __name__ == "__main__":
-    check()
+    check(current_movies)
